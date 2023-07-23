3deaa00aa003a9b49881993b5e13920d351e0142
Jaroslav Švarc <jara.svarc@email.cz>
2023-07-23 16:19:46 +0200

    created simple menu and place to show created components

--- src/Index.fs
+++ src/Index.fs
@@ -5,57 +5,58 @@ open Fable.Remoting.Client
 open Types
 
 type Model =
-    { Data: Value
-      RenderingCode: RenderingCode }
+    { CreatedComponents: RenderingCode list }
+
 
 type Msg =
     | SetData of Value
     | SetRenderingCode of RenderingCode
 
 let example =
-    Sequence [
-        HtmlElement("h1", [], Constant("TODO list"))
-        HtmlList(false, Field("tasks"), Hole)
-    ]
-
-
-
-let ex2 =
-    Sequence [
-        HtmlElement("h1", [], Constant("TODO list"))
-        HtmlList(
-            false,
-            Field("tasks"),
-            Sequence [
-                HtmlElement("input", [ "checked", Reference(Field("completed")) ], Empty)
-                HtmlElement("label", [], Reference(Field("task")))
-            ]
-        )
-    ]
+    Sequence [ HtmlElement("h1", [], Constant("TODO list"))
+               HtmlList(false, Field("tasks"), Hole) ]
 
 let init () : Model * Cmd<Msg> =
-    { Data = Empty
-      RenderingCode = example },
-    Cmd.none
+    { CreatedComponents = [ example ] }, Cmd.none
 
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
-    { Data = Empty
-      RenderingCode = example },
-    Cmd.none
+    { model with CreatedComponents = [ example ] }, Cmd.none
 
 
 open Feliz
 open Feliz.Bulma
 
+let upploadButtonView = Html.button [ Html.text "Upload" ]
 
-
-let view (model: Model) (dispatch: Msg -> unit) =
-    Html.div [
-        Html.h1 [ Html.text "TODO list" ]
-        Html.ul [
-            Html.li [ Html.text "Buy milk" ]
-            Html.li [ Html.text "Buy eggs" ]
-            Html.li [ Html.text "Buy bread" ]
+let sideMenuView =
+    Bulma.box[
+        Bulma.menu [
+            Bulma.menuLabel [
+                Html.text "Menu"
+            ]
+            Bulma.menuList [ upploadButtonView]
         ]
     ]
+
+let createdComponentView (model: Model) =
+    Bulma.box [  Html.text "Created Components"  ]
+let componentCard = Html.div [ Bulma.card [ Html.text "Component Card" ] ]
+let view (model: Model) (dispatch: Msg -> unit) =
+
+        Bulma.columns [
+            prop.children [
+                Bulma.column[
+                    column.is2
+                    prop.children [
+                        sideMenuView
+                    ]
+                ]
+                Bulma.column[
+                    column.is10
+                    prop.children [
+                       //Created components will be shown here
+                    ]
+                ]
+            ]
+        ]
