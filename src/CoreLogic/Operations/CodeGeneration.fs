db8c8eabaf23c7f1152e35e3148d03322add8d1c
Jaroslav Svarc <jara.svarc@email.cz>
2025-01-08 14:07:33 +0100

    tailwind bug fix

--- src/CoreLogic/Operations/CodeGeneration.fs
+++ src/CoreLogic/Operations/CodeGeneration.fs
@@ -187,6 +187,15 @@ let generateFullApp
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+        corePlugins: {
+            preflight: false
+        }
+    }
+    </script>
+
     <title>Sandbox Preview</title>
 </head>
 <body>
